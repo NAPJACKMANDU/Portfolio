0b184dbadb7d8c4abeb97e873f9ff29380d4a6af
NAPJACKMANDU <sjw070283828@gmail.com>
2025-01-13 13:49:04 +0900

    포트폴리오

--- src/main/java/com/yunseo/demo/controller/MainController.java
+++ src/main/java/com/yunseo/demo/controller/MainController.java
@@ -4,13 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.yunseo.demo.service.Mainservice;
 
 @Controller
 public class MainController {
 
-	@Autowired
-	Mainservice service ;
 	
 	@GetMapping("/mandu")
 	public String mainpage() {
