cf25593a2a1ba2d5640c473e8985c9c834477a29
NAPJACKMANDU <sjw070283828@gmail.com>
2025-01-13 11:25:49 +0900

    포트폴리오

--- src/main/java/com/yunseo/demo/PortfolioApplication.java
+++ src/main/java/com/yunseo/demo/PortfolioApplication.java
@@ -2,10 +2,14 @@ package com.yunseo.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class PortfolioApplication {
     public static void main(String[] args) {
         SpringApplication.run(PortfolioApplication.class, args);
     }
+
 }
+	
